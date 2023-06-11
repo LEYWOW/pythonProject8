cd75fa2c1c9fc413861be952dcbbe73db1c9e813
LEYWOW <comfy0423@gmail.com>
2023-06-11 11:34:46 +0200

    homework functiontime

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Час виконання функції {func.__name__}: {execution_time} секунд")
+        return result
+    return wrapper
+
+@measure_time
+def my_function():
+    time.sleep(2)
+    print("Функція виконана")
+
+my_function()
